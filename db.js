9f53485eb6affab03798c0f4d165096c1d0f1b1d
Pierre Genthon <pierre.genthon@wildcodeschool.fr>
2020-06-01 15:05:03 +0200

    db connection pool

--- db.js
+++ db.js
@@ -2,8 +2,17 @@ require('dotenv').config();
 const mysql = require('mysql');
 
 class Database {
-  constructor (connection) {
-    this.connection = connection;
+  init() {
+    this.connection = mysql.createPool({
+      host: process.env.DB_HOST || 'localhost',
+      port: process.env.DB_PORT || '3307',
+      user: process.env.DB_USER || 'root',
+      password: process.env.DB_PASS || 'root',
+      database: process.env.DB_NAME || 'customer_api_database',
+      connectionLimit : 10
+    });
+
+    return this;
   }
 
   query (...args) {
@@ -16,19 +25,4 @@ class Database {
   }
 }
 
-// Create a connection to the database
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST || 'localhost',
-  port: process.env.DB_PORT || '3307',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASS || 'root',
-  database: process.env.DB_NAME || 'customer_api_database'
-});
-
-// open the MySQL connection
-connection.connect(error => {
-  if (error) throw error;
-  console.log('Successfully connected to the database.');
-});
-
-module.exports = new Database(connection);
+module.exports = (new Database()).init();
