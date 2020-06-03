57c2ccbe601eca55fb454c06d170f9ca4d975a42
Pierre Genthon <pierre.genthon@wildcodeschool.fr>
2020-06-03 14:28:51 +0200

    tmp

--- server.js
+++ server.js
@@ -18,8 +18,7 @@ app.use('/', (req, res) => { res.redirect('/customers'); });
 
 // set port, listen for requests
 const server = app.listen(PORT, () => {
-  console.log('Server is running on port ' + PORT);
+  console.log(`Server is running on port ${PORT}`);
 });
-
 
 module.exports = server;
